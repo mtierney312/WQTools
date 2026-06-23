8ac5f0a9e5511df4ca260e3994471af433f13519
mtierney312 <87143100+mtierney312@users.noreply.github.com>
2026-06-23 13:48:48 -0400

    Update mod_flow.R

--- R/mod_flow.R
+++ R/mod_flow.R
@@ -43,6 +43,7 @@ mod_flow_ui <- function(id) {
           numericInput(
             ns("target_drainage_area"),
             "Target Location Drainage Area (sq mi):",
+            value=0,
             min = 0
           ),
           radioButtons(
@@ -235,6 +236,7 @@ mod_flow_server <- function(id, cleaned_storet_data = NULL) {
         numericInput(
           ns(paste0("da_", gage_id)),
           paste("Gage", gage_id, "Drainage Area (sq mi):"),
+          value=0
           min = 0
         )
       })
